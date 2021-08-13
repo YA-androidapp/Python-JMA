fbb5e8282acf38894ebb4544cab5ad1685329053
YA <YA-androidapp@users.noreply.github.com>
2021-08-13 11:17:50 +0900

    updates

--- jma.py
+++ jma.py
@@ -3,6 +3,7 @@
 from collections import OrderedDict
 from datetime import datetime
 import json
+import os
 import requests
 import sys
 
@@ -10,6 +11,14 @@ from const import JMA_ICONS, JMA_ICON_BASEURL, JMA_TELOPS
 
 JSON_URL = 'https://www.jma.go.jp/bosai/forecast/data/forecast/010000.json'
 RESULT_FILE_PATH = 'result.txt'
+
+
+def preparation():
+    if os.path.isfile(RESULT_FILE_PATH):
+        print('REMOVE')
+        os.remove(RESULT_FILE_PATH)
+    else:
+        print('NOT FOUND')
 
 
 def main():
@@ -33,50 +42,51 @@ def main():
     with open(RESULT_FILE_PATH, encoding='utf-8', mode='a') as f:
 
         for area in data:
-            name = area['name']
-            # print('[', name, ']')
+            area_name = area['name']
+            print('[', area_name, ']')
 
             codes = []
             icons = []
+            names_wc = []
+            names_te = []
             temps = []
 
             for ts in area['week']['timeSeries']:
+                ts_name = ts['areas']['area']['name']
+                print('  [', ts_name, ']')
+
                 times = [n for n in ts['timeDefines']]
+
                 if 'weatherCodes' in ts['areas']:
                     for i,v in enumerate(ts['areas']['weatherCodes']):
-                        # print(times[i], ':', JMA_TELOPS[v], end='\t')
+                        print(times[i], ':', JMA_TELOPS[v], end=' ')
                         codes.append(
                             JMA_TELOPS[v]
                         )
                         icons.append(
                             JMA_ICON_BASEURL + JMA_ICONS[v].get('day')
                         )
+                        names_wc.append(ts_name)
+                    print('')
                 if 'tempsMax' in ts['areas']:
                     for i,v in enumerate(ts['areas']['tempsMax']):
-                        # print('\t', ts['areas']['tempsMax'][i], '\t', ts['areas']['tempsMin'][i], end='\t')
-                        temp_max = ts['areas']['tempsMax'][i] if ts['areas']['tempsMax'][i] != '' else ' - '
-                        temp_min = ts['areas']['tempsMin'][i] if ts['areas']['tempsMin'][i] != '' else ' - '
+                        print(ts['areas']['tempsMax'][i], '/', ts['areas']['tempsMin'][i], end=' ')
+                        temp_max = ts['areas']['tempsMax'][i] if ts['areas']['tempsMax'][i] != '' else '-'
+                        temp_min = ts['areas']['tempsMin'][i] if ts['areas']['tempsMin'][i] != '' else '-'
                         temps.append(
-                            temp_max + ' / ' + temp_min
+                            temp_max + '/' + temp_min
                         )
+                        names_te.append(ts_name)
+                    print('')
 
-            if len(codes) > 0 and len(codes) > 0:
+            if len(codes) > 0 and len(temps) > 0:
                 for i in range(len(times)):
-                    # print(
-                    #     name,
-                    #     datetime.strftime(
-                    #         datetime.strptime(
-                    #             times[i],
-                    #             '%Y-%m-%dT%H:%M:%S%z'
-                    #         ),
-                    #         '%m/%d'
-                    #     ),
-                    #     codes[i],
-                    #     temps[i],
-                    #     icons[i]
-                    # )
                     f.write(
-                        name +
+                        area_name +
+                        '\t' +
+                        names_wc[i] +
+                        '\t' +
+                        names_te[i] +
                         '\t' +
                         datetime.strftime(
                             datetime.strptime(
@@ -96,8 +106,10 @@ def main():
 
                 codes = []
                 icons = []
+                names = []
                 temps = []
 
 
 if __name__ == '__main__':
+    preparation()
     main()
